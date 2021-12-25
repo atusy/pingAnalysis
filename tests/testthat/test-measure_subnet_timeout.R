5bff308abe164fa1517019702c77e7efc564cc5c
atusy <30277794+atusy@users.noreply.github.com>
2021-12-25 22:57:06 +0900

    remove unittest which is no longer implemented

--- tests/testthat/test-measure_subnet_timeout.R
+++ tests/testthat/test-measure_subnet_timeout.R
@@ -14,18 +14,6 @@ test_that("find_subnet returns a vector of chracter sized by prefix-size", {
   expect_equal(stringr::str_length(subnet), prefix_sizes)
 })
 
-test_that("find_neighbors should group ip addresses based on subnet.", {
-  expected <- list(
-    c("10.20.30.1/16", "10.20.30.2/16"),
-    c("192.168.1.1/24", "192.168.1.2/24"),
-    c("192.168.0.1/24", "192.168.0.2/24")[]
-  )
-  expect_setequal(
-    find_neighbors(data.frame(address = unlist(expected)))$neighbors_full,
-    expected
-  )
-})
-
 test_that("Measure timeout of switches", {
   basetime <- 20200101000000
   pings <- c(
